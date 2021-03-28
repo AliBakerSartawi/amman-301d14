c6d432928e0a15653b568b223ddfa4eb61521e47
AliBakerSartawi <alisartawi@live.com>
2021-03-28 14:43:52 +0300

    server demo

--- class-06/demo-server.js
+++ class-06/demo-server.js
@@ -10,21 +10,63 @@
 
 'use strict';
 
-// load envirnoment variables from the .env file
+////// load envirnoment variables from the .env file
 require('dotenv').config();
 
-//application dependencies
+//////application dependencies
 const express = require('express');
 const cors = require('cors');
+
+
+
+//////application setup (kickstarting)
 const PORT = 3000; //for convention reasons, it's in all caps
 // another way to reference PORT
-const PORT = process.env.PORT; //this will take the value from .env file
-console.log(process.env.PORT);
-
-//application setup (kickstarting)
-// const PORT = process.env.PORT;
+// const PORT = process.env.PORT; //this will take the value from .env file
+// console.log(process.env.PORT);
 const app = express();
+app.use(cors())
 
+///endpoints
+app.get('/location', handleLocationRequest);
+app.get('/restaurant', handleRestaurantRequest);
 
-// starting point of our application
+function handleLocationRequest(req, res) {
+  console.log(req.query);
+  console.log(req.query.city);
+  const searchQuery = req.query.city;
+
+  const locationsRawData = require('./location.json');
+  const location = new Location(locationsRawData[0]);
+  res.send(location);
+}
+
+function handleRestaurantRequest(req, res) {
+  const restaurantsRawData = require('./restaurants.json'); //require gets the data from .json
+  const restaurantsData = [];
+
+  restaurantsRawData.nearby_restaurants.forEach(restaurant => {
+    restaurantsData.push(new Restaurant(restaurant));
+  });
+
+  res.send(restaurantsData);
+}
+
+// constructors
+//we used the constructor to change the property names to match the mustache template in the HTML
+function Location(data) {
+  this.formatted_query = data.display_name;
+  this.latitude = data.lat;
+  this.longitude = data.lon;
+}
+
+function Restaurant(data) {
+  this.restaurant = data.restaurant.name;
+  this.cuisines = data.restaurant.cuisines;
+  this.locality = data.restaurant.location.locality;
+}
+
+app.use('*', (request, response)=>{
+  response.send('all good, nothing to see here');
+});
 app.listen(PORT, ()=> console.log(`Listening to Port ${PORT}`));
